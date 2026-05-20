615e6ce3f1310c5d7ef40c94fe4a68e5877249e7
Singithi Kandage <singithi.kandage@gmail.com>
2026-05-20 10:52:33 -0400

    feat: updated colors in images

--- src/blocks/CallToActionBlock/Component.tsx
+++ src/blocks/CallToActionBlock/Component.tsx
@@ -10,10 +10,10 @@ const CallToActionBlock: React.FC<CTABlockProps> = ({ cta, line1, line2 }) => {
     <Section className="bg-[var(--neutral-200)] p-[var(--gutter-size)]">
       <div className="flex flex-col gap-[var(--spacing-sm)] md:gap-[var(--spacing-2xl)] items-center text-center">
         <div className="flex flex-col gap-[var(--spacing-sm)] md:gap-[var(--spacing-lg)]">
-          <h2 className="font-normal leading-normal text-xl md:text-3xl text-[color:var(--primary-600)]">
+          <h2 className="font-normal leading-normal text-xl md:text-3xl text-[color:var(--secondary-600)]">
             {line1}
           </h2>
-          <h2 className="font-normal leading-normal text-3xl md:text-4xl italic text-[color:var(--primary-600)]">
+          <h2 className="font-normal leading-normal text-3xl md:text-4xl italic text-[color:var(--secondary-600)]">
             {line2}
           </h2>
         </div>
